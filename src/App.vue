623fac42bf6a46735ecbfddd54037f0ebae05200
warmthsea <2586244885@qq.com>
2023-08-24 10:16:29 +0800

    add test

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,12 @@
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
     <RouterView />
     <TheFooter />
+
+    <TestFather>
+      <template #header>
+        header12
+      </template>
+      test child1ren slot22334 222
+    </TestFather>
   </main>
 </template>
---
+++ src/components/TestFather.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+onMounted(() => {
+  console.warn('onMounted')
+})
+</script>
+
+<template>
+  <div>
+    <h1 mt-10 text-2xl>
+      test father
+    </h1>
+    <slot name="header" />
+    <slot />
+  </div>
+</template>
